34748ad17bca740ef1bfaca5441e12a134c1799c
Mauro Roves <mauro.roves@gmail.com>
2023-03-10 22:46:45 -0300

    correccion primera entrega ReactsJS coderhouse

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,26 +1,13 @@
-import "./container.css";
+ import "./ItemListContainer.css";
 
-const Saludo = () => {
-    function mostrarAlerta() {
-      alert("Hola, como estas?")
-    }
-    
-    return (
-    <div>
-      <Description alerta={mostrarAlerta} />
-    </div>
-    );
-  }
+ function ItemListContainer() {
+  return (     
+        <div className="itemList container-fluid d-flex justify-content-center">
+          <alert>"Hola, como estas?"</alert>
+        </div>
+        );
+}
+
+export default ItemListContainer;
   
-  function Description({ alerta }) {
-    return (
-      <div className="fondo d-flex justify-content-center pt-5 pb-5">
-        <button className="boton" onClick={alerta}>Click me</button>
-      </div>
-    );
-  }
-  
-export default Saludo
-
-
 
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,72 +1,59 @@
-/* const App = () => {
-    function mostrarAlerta() {
-      alert("Sabelo Wachin")
-    }
-    
-    return (
-    <div>
-      <Title value="Hola Mauro" />
-      <Description alerta={mostrarAlerta} />
-    </div>
-    );
-  }
-  
-  function Title({ value }) {
-    return <h1>{value}</h1>;
-  }
-  
-  
-  function Description({ alerta }) {
-    return (
-      <div>
-        <p>Pero mas bien!</p>
-        <button onClick={alerta}>Enviar Alerta</button>
-      </div>
-    );
-  }
-  
-export default App */
 
-import "./navbar.css";
-  
+import "./NavBar.css";
+
+
+
 function NavBar() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-      <div className="container-fluid ">
-        <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown">
-          <ul className="navbar-nav ">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Productos
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Cortador1</a></li>
-                <li><a className="dropdown-item" href="#">Cortador2</a></li>
-                <li><a className="dropdown-item" href="#">Cortador3</a></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Quienes Somos</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contacto</a>
-            </li>
-            <li className="nav-item">
-              <img src="../imagenes/cart.svg" alt="carrito" />
-            </li>
-          </ul>
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+        <div className="container-fluid ">
+          <div className=" container-fluid collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown">
+            <ul className="navbar-nav flex-nowrap align-items-center">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Productos
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Cortador1</a></li>
+                  <li><a className="dropdown-item" href="#">Cortador2</a></li>
+                  <li><a className="dropdown-item" href="#">Cortador3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Quienes Somos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contacto</a>
+              </li>
+              <li className="nav-item">
+              <CartWidget/>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-
-            
+      </nav>
+      
+    
+    
+    </div>        
           );
+    }
+
+     function CartWidget() {
+      return (     
+            <div className=" container-fluid d-flex">
+              <img src="../imagenes/cart.svg" alt="carrito" className="img-fluid" />
+              <p>4</p>  
+            </div>
+            );
     }
   
   
 export default NavBar;
+
 
 
